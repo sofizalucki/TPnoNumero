e630ebab9870e52d4b097932b1a7ee331c524ddc
JOAQUÍN IRIBAS <49627045@ort>
2025-07-03 14:24:14 -0300

    Models

---
+++ TPnoNumero/Models/Integrante.cs
@@ -0,0 +1,12 @@
+namespace TPnoNumero.Models;
+
+public static class Integrante
+{
+    static public int IdIntegrante {get; private set;}
+    static public string nombtrUser { get; private set; }
+    static public string apwllido { get; private set; }
+    static public int DNI { get; private set; }
+    static public string direccion { get; private set; }
+    static public string barrio { get; private set; }
+    static public string password{get; private set;}
+}
